6a8589eab0f53171785f49380138ef8feb7abcc5
George Dainton <gcdainton94@gmail.com>
2022-09-27 14:27:52 +0100

    3.8 Complete

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 app.use(express.json())
+
+app.use(morgan((tokens, req, res) => {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    JSON.stringify(req.body)
+  ].join(' ')
+}))
+
+
 
 let persons = [
   { 
@@ -57,7 +71,7 @@ app.post('/api/persons', (req, res) => {
     return res.status(404).json('Missing name')
   } else if (!body.number) {
     return res.status(404).json('Missing number')
-  } else if (persons.filter(person => person.name === body.name)) {
+  } else if (persons.find(person => person.name === body.name)) {
     return res.status(400).json('Name must be unique')
   }
 
@@ -76,6 +90,12 @@ app.delete('/api/persons/:id', (req, res) => {
   persons = persons.filter(person => person.id !== id)
   res.status(204).end()
 })
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'Unknown Endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT)
